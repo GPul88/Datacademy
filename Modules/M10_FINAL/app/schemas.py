af9eca6b5af02bd27b3a4704e60403aefdd625ae
JoepBeurskens <joepbeurskens@hotmail.com>
2023-01-05 10:11:01 +0100

    first working version M10

--- Modules/M10_FINAL/app/schemas.py
+++ Modules/M10_FINAL/app/schemas.py
@@ -1,10 +1,10 @@
-import datetime as _dt
-import pydantic as _pydantic
+import datetime as dt
+import pydantic as pydantic
 
-class _AuctionBase(_pydantic.BaseModel):
+class _AuctionBase(pydantic.BaseModel):
     relatedCompany: str
-    auctionStart: _dt.datetime
-    auctionEnd: _dt.datetime
+    auctionStart: dt.datetime
+    auctionEnd: dt.datetime
     branchCategory: str
 
 class AuctionCreate(_AuctionBase):
@@ -17,7 +17,7 @@ class Auction(_AuctionBase):
         orm_mode = True
 
 
-class _LotBase(_pydantic.BaseModel):
+class _LotBase(pydantic.BaseModel):
     auctionID: int
     numberOfItems: int
     estimatedValue: float
@@ -30,34 +30,47 @@ class LotCreate(_LotBase):
 class Lot(_LotBase):
     lotNr: int
     countryCode: str
-    saleDate: _dt.datetime
+    saleDate: dt.datetime
     startingBid: float
     suffix: str
     buyerAccountID: int
     currentBid: float
-    VAT: int
+    VAT: float
     sold: bool
 
     class Config:
         orm_mode = True
 
 
-class _BidBase(_pydantic.BaseModel):
+class _BidBase(pydantic.BaseModel):
     auctionID: int
     lotNr: int
     isCombination: bool
     accountID: int
     isCompany: bool
     bidPrice: float
-    biddingDateTime: _dt.datetime
+    biddingDateTime: dt.datetime
 
 class BidCreate(_BidBase):
     pass
 
 class Bid(_BidBase):
     bidNr: int
-    closingDateTime: _dt.datetime
+    closingDateTime: dt.datetime
 
     class Config:
         orm_mode = True
 
+
+class _StartingPriceBase(pydantic.BaseModel):
+    numberOfItems:int
+    estimatedValue:float
+    reserveBid:float
+    branchCategory:str
+    auctionDuration:float
+
+class Price(_StartingPriceBase):
+    best_starting_bid_price: float
+
+    class Config:
+        orm_mode = True
