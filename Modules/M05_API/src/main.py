3155f19c6bbb37f4f072f605fb84287199380c5d
RemcoCoppens <remcocoppens@outlook.com>
2023-02-07 15:33:04 +0100

    Live ready

--- Modules/M05_API/src/main.py
+++ Modules/M05_API/src/main.py
@@ -28,19 +28,15 @@ def get_customer(customerId: int):
 #to make
 @app.get("/get-customer-by-name/{lastName}")
 def get_customer_by_name(lastName: str):
-    for customerId in customers:
-        if customers[customerId]['lastName'] == lastName:
-            return customers[customerId]
-
-    return {"Error", f"Customer with last name: '{lastName}' does not exists"}
+    ...
 
 
 @app.get("/get-customers/")
 def get_customers(skip: int = 0, limit: int = 3):
-    return {i: customers[i] for i in range(skip, min(skip+limit, len(customers)))}
+    ...
 
 
-# API POST Request(s) ####
+# API POST Request(s)
 @app.post("/create-customer/{customerId}")
 def create_customer(customerId: int, firstName: str, lastName: str, address: str):
     if customerId in customers:
@@ -58,15 +54,7 @@ def create_customer(customerId: int, firstName: str, lastName: str, address: str
 
 #to make
 @app.post("/create-customer-auto-increment/")
-def create_customer_autoincrement(firstName: str, lastName: str, address: str):
-    customerId = max(customers.keys()) + 1
-
-    customers[customerId] = {
-        "firstName": firstName,
-        "lastName": lastName,
-        "address": address
-    }
-    return customers[customerId]
+...
 
 
 # API PUT Request(s) ####
@@ -79,13 +67,7 @@ def update_customer_address(customerId: int, address: str):
     return customers[customerId]
 
 #to make
-@app.put("/update-customer-address-by-name/")
-def update_customer_address_by_name(firstName: str, lastName: str, address: str):
-    for customerId in customers:
-        if customers[customerId]['firstName'] == firstName and customers[customerId]['lastName'] == lastName:
-            customers[customerId]['address'] = address
-
-            return customers[customerId]
+...
 
 
 # API DELETE Request(s) ####
@@ -98,16 +80,4 @@ def delete_customer(customerId: int):
     return {"Message": f"Customer {customerId} deleted successfully."}
 
 #to make
-@app.delete("/delete-customer-by-name/")
-def delete_customer_by_name(firstName: str, lastName: str):
-    foundCustomer = False
-    for customer in customers:
-        if customers[customer]['firstName'] == firstName and customers[customer]['lastName'] == lastName:
-            foundCustomer = True
-            del customers[customer]
-            break
-
-    if not foundCustomer:
-        return {"Error": "The customer you are trying to delete does not exist."}
-    else:
-        return {"Message": f"Customer {firstName} {lastName} deleted successfully."}
+...
